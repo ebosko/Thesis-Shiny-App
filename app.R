4abd8d206f429038888dd6c411d50127eabfc395
ebosko <edbosko@gmail.com>
2025-04-21 18:31:05 -0600

    initial commit

--- app.R
+++ app.R
@@ -1,53 +1,60 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-# load libraries
 library(shiny)
 library(tidyverse)
 library(readxl)
+library(MASS)
+library(ggplot2)
 
-# Define UI for application that draws a histogram
+#load data
+#read in data
+tib <- read_excel('C:/Users/Eddie/OneDrive/Desktop/BIOSTAT/Thesis/Thesis/DataProcessed/2025.4.2.xlsx', sheet = "tib.long")
+
+ggo <- read_excel('C:/Users/Eddie/OneDrive/Desktop/BIOSTAT/Thesis/Thesis/DataProcessed/2025.4.2.xlsx', sheet = "ggo.long")
+
+cons <- read_excel('C:/Users/Eddie/OneDrive/Desktop/BIOSTAT/Thesis/Thesis/DataProcessed/2025.4.2.xlsx', sheet = "cons.long")
+
+bronch <- read_excel('C:/Users/Eddie/OneDrive/Desktop/BIOSTAT/Thesis/Thesis/DataProcessed/2025.4.2.xlsx', sheet = "bronch.long")
+
+atel <- read_excel('C:/Users/Eddie/OneDrive/Desktop/BIOSTAT/Thesis/Thesis/DataProcessed/2025.4.2.xlsx', sheet = "atel.long")
+
+ln <- read_excel('C:/Users/Eddie/OneDrive/Desktop/BIOSTAT/Thesis/Thesis/DataProcessed/2025.4.2.xlsx', sheet = "ln.long")
+
+thin <- read_excel('C:/Users/Eddie/OneDrive/Desktop/BIOSTAT/Thesis/Thesis/DataProcessed/2025.4.2.xlsx', sheet = "thin.long")
+
+thick <- read_excel('C:/Users/Eddie/OneDrive/Desktop/BIOSTAT/Thesis/Thesis/DataProcessed/2025.4.2.xlsx', sheet = "thick.long")
+
+demo <- read.csv('C:/Users/Eddie/OneDrive/Desktop/BIOSTAT/Thesis/Thesis/DataRaw/demographic_final.csv')
+
+# UI
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  titlePanel("CT Score Distribution by Lobe"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("feature", "Choose a CT Feature:",
+                  choices = c("Bronchiectasis", "Atelectasis", "GGO", "TIB", "Consolidation"))
+    ),
+    mainPanel(
+      plotOutput("scorePlot")
     )
+  )
 )
 
-# Define server logic required to draw a histogram
+# Server
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  
+  output$scorePlot <- renderPlot({
+    tib %>%
+      filter(feature == input$feature) %>%
+      ggplot(aes(x = Attribute, fill = factor(Value))) +
+      geom_bar(position = "stack") +
+      labs(
+        title = paste("Distribution of", input$feature, "Ratings by Lobe"),
+        x = "Lobe",
+        y = "Count",
+        fill = "Rating"
+      ) +
+      theme_minimal()
+  })
 }
 
-# Run the application 
+# Run the app
 shinyApp(ui = ui, server = server)
